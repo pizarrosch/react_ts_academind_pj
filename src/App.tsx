5683aba2abf132626aa2b1bca9f16643875e942e
pizarrosch <bayernsch89@gmail.com>
2024-11-24 23:52:14 +0100

    Commit_3: Make the list item as a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
-  return (
-    <div>
-      <Todos items={['Learn React', 'Learn TypeScript']}/>
-    </div>
-  );
+    const todos = [
+        new Todo('Learn React'),
+        new Todo('Learn TypeScript')
+    ]
+
+    return (
+        <div>
+            <Todos items={todos}/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import Todo from "../models/todo";
+import TodoListItem from "./TodoListItem";
 
-const Todos: React.FC<{items: string[]}> = (props) => {
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
     return (
         <ul>
             {props.items.map((item) => (
-                    <li key={item}>{item}</li>
+                <TodoListItem key={item.id} text={item.text}/>
             ))}
         </ul>
     )
 }
 
- export default Todos;
+export default Todos;
